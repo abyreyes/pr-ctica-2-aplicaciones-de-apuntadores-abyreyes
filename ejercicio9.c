8dc07a8e6d80698d5b11d8747e00edeaa915a419
abyreyes <aby.08reyes@gmail.com>
2025-03-01 20:33:15 -0600

    Update ejercicio9.c

--- ejercicio9.c
+++ ejercicio9.c
@@ -3,11 +3,20 @@
 
 int sumaArreglo(int *arr, int tamaño) {
     // Implementar la suma aquí
-    return 0;
+    int suma = 0;
+    for (int i = 0; i < tamaño; i++) {
+        suma += *(arr + i); // Usamos aritmética de apuntadores
+    }
+    return suma;
 }
 
 int main() {
     int numeros[] = {1, 2, 3, 4, 5};
     // Código para calcular y mostrar la suma
+    int tamaño = sizeof(numeros) / sizeof(numeros[0]);
+
+    int resultado = sumaArreglo(numeros, tamaño);
+    printf("La suma de los elementos del arreglo es: %d\n", resultado);
+    
     return 0;
 }
